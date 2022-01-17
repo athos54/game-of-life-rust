97e0df57549ad4d145d92b3e0b4bcc680cbb7509
Athos Orío <athos.oc@gmail.com>
2022-01-17 14:09:34 +0100

    work but wrong

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+use rand::{thread_rng, Rng};
+
 #[derive(Debug)]
 #[warn(dead_code)]
 struct Table {
@@ -18,23 +20,44 @@ impl Table {
  }
 
  fn next_day(&mut self)  {
-    // self
     let mut cell = &mut self.rows[0].cells[0];
-    cell.state = STATE::DEAD;
-    println!("next day {:#?}", &self.rows[0].cells[0].state)
+    cell.state = 0;
+
+    for x in 0..self.rows.len(){
+        for y in 0..self.rows[x].cells.len() {
+            let life_around = self.rows[x].cells[y].check(x, y, self);
+            // this not work because is changing stage between checks, I should create a new table
+            self.rows[x].cells[y].state = 0;
+            if self.rows[x].cells[y].state == 0 {
+                if life_around == 3 {
+                    self.rows[x].cells[y].state = 1
+                }
+            }else{
+                if life_around != 2 && life_around != 3 {
+                    self.rows[x].cells[y].state = 0
+                }
+            }
+
+        }
+    }
+
  }
 
  fn print(&self){
+    println!("");
+
     for ri in 0..self.rows.len() {
         for ci in 0..self.rows[ri].cells.len()   {
-            match &self.rows[ri].cells[ci].state {
-                STATE::ALIVE => print!("😀"),
-                STATE::DEAD => print!("💩"),
+            if self.rows[ri].cells[ci].state == 1 {
+                print!("😀");
+            }else{
+                print!("💩");
             }
         }
         println!("")
     }
  }
+
 }
 #[derive(Debug)]
 struct Row {
@@ -43,24 +66,83 @@ struct Row {
 impl Row {
     fn new(size:usize)->Row{
         let mut row = Row { cells: Vec::new() };
-        for i in 0..size {
-            if i==2{
-                row.cells.push(Cell{state:STATE::DEAD})
+        for _ in 0..size {
+            let is_alive = thread_rng().gen_bool(1.0 / 2.0);
+
+            if is_alive {
+                row.cells.push(Cell{state:1})
             }else{
-                row.cells.push(Cell{state:STATE::ALIVE})
+                row.cells.push(Cell{state:0})
             }
         }
         row
     }
+
+    
 }
-#[derive(Debug)]
-enum STATE{
-    ALIVE,
-    DEAD
-}
+// #[derive(Debug)]
+// enum STATE{
+//     ALIVE=1,
+//     DEAD=0
+// }
 #[derive(Debug)]
 struct Cell{
-    state:STATE
+    state:i32
+}
+
+impl Cell{
+    fn check(&self, x:usize,y:usize,table:&Table) -> i32{
+        let size = table.rows.len()-1;
+        
+        let mut total_live = 0;
+
+    
+        if x > 0 && x < size && y > 0 && y < size {
+            // internal cells
+            total_live = total_live + table.rows[x-1].cells[y-1].state;
+            total_live = total_live + table.rows[x-1].cells[y].state;
+            total_live = total_live + table.rows[x-1].cells[y+1].state;
+    
+            total_live = total_live + table.rows[x+1].cells[y-1].state;
+            total_live = total_live + table.rows[x+1].cells[y].state;
+            total_live = total_live + table.rows[x+1].cells[y+1].state;
+    
+            total_live = total_live + table.rows[x].cells[y-1].state;
+            total_live = total_live + table.rows[x].cells[y+1].state;
+
+        }else {
+            //external cells
+            if x == 0 {
+                if y == 0 {
+                    total_live = total_live + table.rows[x+1].cells[y].state;
+                    total_live = total_live + table.rows[x+1].cells[y+1].state;
+                    total_live = total_live + table.rows[x].cells[y+1].state;
+                }
+                if y == size {
+            
+                    total_live = total_live + table.rows[x+1].cells[y-1].state;
+                    total_live = total_live + table.rows[x+1].cells[y].state;
+                    total_live = total_live + table.rows[x].cells[y-1].state;
+                }
+            }
+
+            if x == size {
+                if y == 0 {
+                    total_live = total_live + table.rows[x-1].cells[y].state;
+                    total_live = total_live + table.rows[x-1].cells[y+1].state;
+                    total_live = total_live + table.rows[x].cells[y+1].state;
+                }
+                if y == size {
+                    total_live = total_live + table.rows[x-1].cells[y-1].state;
+                    total_live = total_live + table.rows[x-1].cells[y].state;
+                    total_live = total_live + table.rows[x].cells[y-1].state;
+                }
+            }
+        }
+
+        total_live
+        // STATE::DEAD
+    }
 }
 
 fn clear_terminal (){
@@ -70,7 +152,7 @@ fn main() {
     clear_terminal();
     println!("Hello, world!");
 
-    let mut table = Table::new(10);
+    let mut table = Table::new(60);
     table.print();
     table.next_day();
     table.print();
