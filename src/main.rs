ebe27c2c027ee6c4a94c4244ee40bf19aa33346e
Athos Orío <athos.oc@gmail.com>
2022-01-18 12:32:44 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-use std::env;
 use game_of_life2::game::Table;
+use std::env;
 use std::{thread, time};
 
-fn clear_terminal (){
+fn clear_terminal() {
     std::process::Command::new("clear").status().unwrap();
 }
 
-fn sleep(milis:u64){
+fn sleep(milis: u64) {
     let _time = time::Duration::from_millis(milis);
     thread::sleep(_time);
 }
 
 fn main() {
-    
     let size = get_table_size();
     let speed = get_speed();
 
@@ -20,7 +19,7 @@ fn main() {
     println!("Hello, world!");
     let mut gen = 0;
     let mut table = Table::new(size);
-    
+
     loop {
         clear_terminal();
         println!("Generation: {}", gen);
@@ -33,53 +32,50 @@ fn main() {
             break;
         }
         gen = gen + 1;
-
     }
-
 }
 
-fn get_speed() -> u64{
+fn get_speed() -> u64 {
     let args: Vec<String> = env::args().collect();
-    
-    let speed:String;
+
+    let speed: String;
     if args.len() < 3 {
         speed = "1000".to_string();
-    }else{
+    } else {
         speed = args[2].clone();
     }
 
-    let speed:u64 = match speed.parse::<u64>() {
+    let speed: u64 = match speed.parse::<u64>() {
         Ok(num) => {
             if num >= 3 {
                 num
             } else {
                 5
             }
-        },
+        }
         Err(_) => 10,
     };
     speed
 }
 
-
-fn get_table_size() -> usize{
+fn get_table_size() -> usize {
     let args: Vec<String> = env::args().collect();
-    
-    let size:String;
+
+    let size: String;
     if args.len() < 2 {
         size = "3".to_string();
-    }else{
+    } else {
         size = args[1].clone();
     }
 
-    let size:usize = match size.parse::<usize>() {
+    let size: usize = match size.parse::<usize>() {
         Ok(num) => {
             if num >= 3 {
                 num
             } else {
                 5
             }
-        },
+        }
         Err(_) => 10,
     };
     size
