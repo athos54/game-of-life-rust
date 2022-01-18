ea1d8be79891bb6805fc649c128adfb31e2b0076
Athos Orío <athos.oc@gmail.com>
2022-01-18 12:32:17 +0100

    improves

---
+++ src/lib.rs
@@ -0,0 +1,239 @@
+pub mod game {
+    use rand::{thread_rng, Rng};
+
+    #[derive(Debug,Clone)]
+    #[warn(dead_code)]
+    pub struct Table {
+        pub rows: Vec<Row>
+    }
+    
+    impl Table {
+     pub fn new(size:usize) -> Table{
+        println!("Building new table with size {}",size);
+        let mut rows:Vec<Row> = Vec::new();
+        for _ in 0..size {
+            rows.push(Row::new(size));
+        }
+        let table = Table{rows:rows};
+    
+        table
+     }
+    
+     pub fn next_day(&mut self) -> bool {
+        let mut table2 = self.clone();
+        let mut is_some_cell_alive = false;
+        for x in 0..self.rows.len(){
+            for y in 0..self.rows[x].cells.len() {
+                let life_around = self.rows[x].cells[y].check(x, y, self);
+                // this not work because is changing stage between checks, I should create a new table
+                // self.rows[x].cells[y].state = 0;
+                if self.rows[x].cells[y].state == 0 {
+                    if life_around == 3 {
+                        is_some_cell_alive = true;
+                        table2.rows[x].cells[y].state = 1
+                    }else{
+                        table2.rows[x].cells[y].state = 0
+                    }
+                }else{
+                    if life_around != 2 && life_around != 3 {
+                        table2.rows[x].cells[y].state = 0
+                    }else{
+                        is_some_cell_alive = true;
+                        table2.rows[x].cells[y].state = 1
+                    }
+                }
+    
+            }
+        }
+    
+        self.rows = table2.rows;
+        !is_some_cell_alive
+     }
+    
+     pub fn print(&self){
+        println!("");
+    
+        for ri in 0..self.rows.len() {
+            for ci in 0..self.rows[ri].cells.len()   {
+                if self.rows[ri].cells[ci].state == 1 {
+                    print!("😀");
+                }else{
+                    print!("💩");
+                }
+            }
+            println!("")
+        }
+     }
+    
+    }
+    #[derive(Debug,Clone)]
+    pub struct Row {
+        pub cells: Vec<Cell>
+    }
+    impl Row {
+        fn new(size:usize)->Row{
+            let mut row = Row { cells: Vec::new() };
+            for _ in 0..size {
+                let is_alive = thread_rng().gen_bool(1.0 / 2.0);
+    
+                if is_alive {
+                    row.cells.push(Cell{state:1})
+                }else{
+                    row.cells.push(Cell{state:0})
+                }
+            }
+            row
+        }
+    
+        
+    }
+    // #[derive(Debug)]
+    // enum STATE{
+    //     ALIVE=1,
+    //     DEAD=0
+    // }
+    #[derive(Debug,Clone)]
+    pub struct Cell{
+        pub state:i32
+    }
+    
+    impl Cell{
+        fn check(&self, x:usize,y:usize,table:&Table) -> i32{
+            let size = table.rows.len()-1;
+            let mut total_live = 0;
+            
+            if x > 0 && x < size && y > 0 && y < size {
+                // internal cells
+                total_live = total_live + table.rows[x-1].cells[y-1].state;
+                total_live = total_live + table.rows[x-1].cells[y].state;
+                total_live = total_live + table.rows[x-1].cells[y+1].state;
+        
+                total_live = total_live + table.rows[x+1].cells[y-1].state;
+                total_live = total_live + table.rows[x+1].cells[y].state;
+                total_live = total_live + table.rows[x+1].cells[y+1].state;
+        
+                total_live = total_live + table.rows[x].cells[y-1].state;
+                total_live = total_live + table.rows[x].cells[y+1].state;
+    
+            }else {
+                //external cells
+                if x == 0 {
+                    if y == 0 {
+                        total_live = total_live + table.rows[x+1].cells[y].state;
+                        total_live = total_live + table.rows[x+1].cells[y+1].state;
+                        total_live = total_live + table.rows[x].cells[y+1].state;
+                    }
+                    if y > 0 && y < size {
+                        total_live = total_live + table.rows[x+1].cells[y-1].state;
+                        total_live = total_live + table.rows[x+1].cells[y].state;
+                        total_live = total_live + table.rows[x+1].cells[y+1].state;
+                
+                        total_live = total_live + table.rows[x].cells[y-1].state;
+                        total_live = total_live + table.rows[x].cells[y+1].state;
+                    }
+                    if y == size {
+                
+                        total_live = total_live + table.rows[x+1].cells[y-1].state;
+                        total_live = total_live + table.rows[x+1].cells[y].state;
+                        total_live = total_live + table.rows[x].cells[y-1].state;
+                    }
+                }
+    
+                if x == size {
+                    if y == 0 {
+                        total_live = total_live + table.rows[x-1].cells[y].state;
+                        total_live = total_live + table.rows[x-1].cells[y+1].state;
+                        total_live = total_live + table.rows[x].cells[y+1].state;
+                    }
+                    if y > 0 && y < size {
+                        total_live = total_live + table.rows[x-1].cells[y-1].state;
+                        total_live = total_live + table.rows[x-1].cells[y].state;
+                        total_live = total_live + table.rows[x-1].cells[y+1].state;
+                
+                        total_live = total_live + table.rows[x].cells[y-1].state;
+                        total_live = total_live + table.rows[x].cells[y+1].state;
+                    }
+                    if y == size {
+                        total_live = total_live + table.rows[x-1].cells[y-1].state;
+                        total_live = total_live + table.rows[x-1].cells[y].state;
+                        total_live = total_live + table.rows[x].cells[y-1].state;
+                    }
+                }
+    
+                if x > 0 && x < size {
+                    if y == 0 {
+                        total_live = total_live + table.rows[x-1].cells[y].state;
+                        total_live = total_live + table.rows[x-1].cells[y+1].state;
+                
+                        total_live = total_live + table.rows[x+1].cells[y].state;
+                        total_live = total_live + table.rows[x+1].cells[y+1].state;
+                
+                        total_live = total_live + table.rows[x].cells[y+1].state;
+                    }
+                    if y == size {
+                        total_live = total_live + table.rows[x-1].cells[y-1].state;
+                        total_live = total_live + table.rows[x-1].cells[y].state;
+                
+                        total_live = total_live + table.rows[x+1].cells[y-1].state;
+                        total_live = total_live + table.rows[x+1].cells[y].state;
+                
+                        total_live = total_live + table.rows[x].cells[y-1].state;
+                    }
+                    
+                }
+            }
+    
+            total_live
+            // STATE::DEAD
+        }
+    }
+    
+
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::game::Table;
+
+    #[test]
+    fn size_should_be_the_same_between_rows_and_columns() {
+        let size = 3;
+        let table = Table::new(size);
+        assert_eq!(table.rows[0].cells.len(), size);
+        assert_eq!(table.rows.len(), size);
+    }
+    
+    #[test]
+    fn should_calculate_next_day_correctly() {
+        let size = 3;
+        let mut table = Table::new(size);
+        
+        table.rows[0].cells[0].state = 0;
+        table.rows[0].cells[1].state = 0;
+        table.rows[0].cells[2].state = 1;
+        
+        table.rows[1].cells[0].state = 1;
+        table.rows[1].cells[1].state = 0;
+        table.rows[1].cells[2].state = 0;
+        
+        table.rows[2].cells[0].state = 1;
+        table.rows[2].cells[1].state = 0;
+        table.rows[2].cells[2].state = 0;
+
+        table.next_day();
+
+        assert_eq!(table.rows[0].cells[0].state, 0);
+        assert_eq!(table.rows[0].cells[1].state, 0);
+        assert_eq!(table.rows[0].cells[2].state, 0);
+        
+        assert_eq!(table.rows[1].cells[0].state, 0);
+        assert_eq!(table.rows[1].cells[1].state, 1);
+        assert_eq!(table.rows[1].cells[2].state, 0);
+        
+        assert_eq!(table.rows[2].cells[0].state, 0);
+        assert_eq!(table.rows[2].cells[1].state, 0);
+        assert_eq!(table.rows[2].cells[2].state, 0);
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,192 +1,6 @@
-use std::{thread, time};
-use rand::{thread_rng, Rng};
 use std::env;
-
-#[derive(Debug,Clone)]
-#[warn(dead_code)]
-struct Table {
-    rows: Vec<Row>
-}
-
-impl Table {
- fn new(size:usize) -> Table{
-    println!("Building new table with size {}",size);
-    let mut rows:Vec<Row> = Vec::new();
-    for _ in 0..size {
-        rows.push(Row::new(size));
-    }
-    let table = Table{rows:rows};
-
-    table
- }
-
- fn next_day(&mut self)  {
-    let mut table2 = self.clone();
-    for x in 0..self.rows.len(){
-        for y in 0..self.rows[x].cells.len() {
-            let life_around = self.rows[x].cells[y].check(x, y, self);
-            // this not work because is changing stage between checks, I should create a new table
-            // self.rows[x].cells[y].state = 0;
-            if self.rows[x].cells[y].state == 0 {
-                if life_around == 3 {
-                    table2.rows[x].cells[y].state = 1
-                }else{
-                    table2.rows[x].cells[y].state = 0
-                }
-            }else{
-                if life_around != 2 && life_around != 3 {
-                    table2.rows[x].cells[y].state = 0
-                }else{
-                    table2.rows[x].cells[y].state = 1
-                }
-            }
-
-        }
-    }
-
-    self.rows = table2.rows
-
- }
-
- fn print(&self){
-    println!("");
-
-    for ri in 0..self.rows.len() {
-        for ci in 0..self.rows[ri].cells.len()   {
-            if self.rows[ri].cells[ci].state == 1 {
-                // print!("1 ");
-                print!("😀");
-            }else{
-                // print!("0 ");
-                print!("💩");
-            }
-        }
-        println!("")
-    }
- }
-
-}
-#[derive(Debug,Clone)]
-struct Row {
-    cells: Vec<Cell>
-}
-impl Row {
-    fn new(size:usize)->Row{
-        let mut row = Row { cells: Vec::new() };
-        for _ in 0..size {
-            let is_alive = thread_rng().gen_bool(1.0 / 2.0);
-
-            if is_alive {
-                row.cells.push(Cell{state:1})
-            }else{
-                row.cells.push(Cell{state:0})
-            }
-        }
-        row
-    }
-
-    
-}
-// #[derive(Debug)]
-// enum STATE{
-//     ALIVE=1,
-//     DEAD=0
-// }
-#[derive(Debug,Clone)]
-struct Cell{
-    state:i32
-}
-
-impl Cell{
-    fn check(&self, x:usize,y:usize,table:&Table) -> i32{
-        let size = table.rows.len()-1;
-        let mut total_live = 0;
-        
-        if x > 0 && x < size && y > 0 && y < size {
-            // internal cells
-            total_live = total_live + table.rows[x-1].cells[y-1].state;
-            total_live = total_live + table.rows[x-1].cells[y].state;
-            total_live = total_live + table.rows[x-1].cells[y+1].state;
-    
-            total_live = total_live + table.rows[x+1].cells[y-1].state;
-            total_live = total_live + table.rows[x+1].cells[y].state;
-            total_live = total_live + table.rows[x+1].cells[y+1].state;
-    
-            total_live = total_live + table.rows[x].cells[y-1].state;
-            total_live = total_live + table.rows[x].cells[y+1].state;
-
-        }else {
-            //external cells
-            if x == 0 {
-                if y == 0 {
-                    total_live = total_live + table.rows[x+1].cells[y].state;
-                    total_live = total_live + table.rows[x+1].cells[y+1].state;
-                    total_live = total_live + table.rows[x].cells[y+1].state;
-                }
-                if y > 0 && y < size {
-                    total_live = total_live + table.rows[x+1].cells[y-1].state;
-                    total_live = total_live + table.rows[x+1].cells[y].state;
-                    total_live = total_live + table.rows[x+1].cells[y+1].state;
-            
-                    total_live = total_live + table.rows[x].cells[y-1].state;
-                    total_live = total_live + table.rows[x].cells[y+1].state;
-                }
-                if y == size {
-            
-                    total_live = total_live + table.rows[x+1].cells[y-1].state;
-                    total_live = total_live + table.rows[x+1].cells[y].state;
-                    total_live = total_live + table.rows[x].cells[y-1].state;
-                }
-            }
-
-            if x == size {
-                if y == 0 {
-                    total_live = total_live + table.rows[x-1].cells[y].state;
-                    total_live = total_live + table.rows[x-1].cells[y+1].state;
-                    total_live = total_live + table.rows[x].cells[y+1].state;
-                }
-                if y > 0 && y < size {
-                    total_live = total_live + table.rows[x-1].cells[y-1].state;
-                    total_live = total_live + table.rows[x-1].cells[y].state;
-                    total_live = total_live + table.rows[x-1].cells[y+1].state;
-            
-                    total_live = total_live + table.rows[x].cells[y-1].state;
-                    total_live = total_live + table.rows[x].cells[y+1].state;
-                }
-                if y == size {
-                    total_live = total_live + table.rows[x-1].cells[y-1].state;
-                    total_live = total_live + table.rows[x-1].cells[y].state;
-                    total_live = total_live + table.rows[x].cells[y-1].state;
-                }
-            }
-
-            if x > 0 && x < size {
-                if y == 0 {
-                    total_live = total_live + table.rows[x-1].cells[y].state;
-                    total_live = total_live + table.rows[x-1].cells[y+1].state;
-            
-                    total_live = total_live + table.rows[x+1].cells[y].state;
-                    total_live = total_live + table.rows[x+1].cells[y+1].state;
-            
-                    total_live = total_live + table.rows[x].cells[y+1].state;
-                }
-                if y == size {
-                    total_live = total_live + table.rows[x-1].cells[y-1].state;
-                    total_live = total_live + table.rows[x-1].cells[y].state;
-            
-                    total_live = total_live + table.rows[x+1].cells[y-1].state;
-                    total_live = total_live + table.rows[x+1].cells[y].state;
-            
-                    total_live = total_live + table.rows[x].cells[y-1].state;
-                }
-                
-            }
-        }
-
-        total_live
-        // STATE::DEAD
-    }
-}
+use game_of_life2::game::Table;
+use std::{thread, time};
 
 fn clear_terminal (){
     std::process::Command::new("clear").status().unwrap();
@@ -196,21 +10,11 @@ fn sleep(milis:u64){
     let _time = time::Duration::from_millis(milis);
     thread::sleep(_time);
 }
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
     
-    let size = &args[1];
-    
-    let size:usize = match size.parse::<usize>() {
-        Ok(num) => {
-            if num >= 3 {
-                num
-            } else {
-                5
-            }
-        },
-        Err(_) => 10,
-    };
+    let size = get_table_size();
+    let speed = get_speed();
 
     clear_terminal();
     println!("Hello, world!");
@@ -221,9 +25,62 @@ fn main() {
         clear_terminal();
         println!("Generation: {}", gen);
         table.print();
-        sleep(500);
-        table.next_day();
-        gen=gen+1;
+        sleep(speed);
+        let is_last_day = table.next_day();
+        println!("is_last_day {}", is_last_day);
+        if is_last_day {
+            table.print();
+            break;
+        }
+        gen = gen + 1;
+
     }
 
+}
+
+fn get_speed() -> u64{
+    let args: Vec<String> = env::args().collect();
+    
+    let speed:String;
+    if args.len() < 3 {
+        speed = "1000".to_string();
+    }else{
+        speed = args[2].clone();
+    }
+
+    let speed:u64 = match speed.parse::<u64>() {
+        Ok(num) => {
+            if num >= 3 {
+                num
+            } else {
+                5
+            }
+        },
+        Err(_) => 10,
+    };
+    speed
+}
+
+
+fn get_table_size() -> usize{
+    let args: Vec<String> = env::args().collect();
+    
+    let size:String;
+    if args.len() < 2 {
+        size = "3".to_string();
+    }else{
+        size = args[1].clone();
+    }
+
+    let size:usize = match size.parse::<usize>() {
+        Ok(num) => {
+            if num >= 3 {
+                num
+            } else {
+                5
+            }
+        },
+        Err(_) => 10,
+    };
+    size
 }
